e77d507318e167448665cc3d50609588940c01db
kondabalapradeep <68560029+kondabalapradeep@users.noreply.github.com>
2020-07-21 23:36:03 +0530

    Add files via upload

---
+++ assignment day 2.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+bala = 10
+
+
+# In[2]:
+
+
+bala
+
+
+# In[3]:
+
+
+bala
+
+
+# In[4]:
+
+
+bala
+
+
+# In[5]:
+
+
+hi='bala'
+
+
+# In[6]:
+
+
+hi
+
+
+# 
+
+# In[7]:
+
+
+print(1)
+
+
+# In[8]:
+
+
+print ("bala")
+
+
+# In[9]:
+
+
+bala
+
+
+# In[10]:
+
+
+a=1
+b=3
+a+b
+
+objects and datastructures
+# In[11]:
+
+
+type(a
+    )
+
+
+# In[12]:
+
+
+type("bala")
+
+
+# In[13]:
+
+
+d=12.3
+
+
+# In[14]:
+
+
+type(d)
+
+
+# In[15]:
+
+
+c=[bala,hi,ce]
+
+
+# In[16]:
+
+
+bala
+
+
+# In[17]:
+
+
+bala='hi'
+
+
+# In[18]:
+
+
+bala
+
+
+# In[19]:
+
+
+get_ipython().run_line_magic('config', 'IPCompleter.greedy=True')
+
+
+# In[20]:
+
+
+[].type
+
+
+# In[21]:
+
+
+bala.replace
+
+
+# In[22]:
+
+
+bala.count
+
+
+# In[23]:
+
+
+bala.count()
+
+
+# In[24]:
+
+
+bala.upper
+
+
+# In[25]:
+
+
+bala.count
+
+
+# In[26]:
+
+
+bala.count("shdljsdn")
+
+
+# In[27]:
+
+
+bala.count("hihdodjiqd")
+
+
+# In[28]:
+
+
+bala
+
+
+# In[29]:
+
+
+bala.count("h")
+
+
+# In[30]:
+
+
+bala = bala +"hhhhhhhhhhhhhhhhhhhhhhihhhhhhhhhhhhh"
+
+
+# In[31]:
+
+
+bala.count("h")
+
+
+# In[34]:
+
+
+bala.count("hihh")
+
+
+# In[36]:
+
+
+bala
+
+
+# In[37]:
+
+
+bala = bala +"hhhhhhhhhhhhhhhhhhhhhhihhhhhhhhhhhhh"
+
+
+# In[38]:
+
+
+bala
+
+
+# In[39]:
+
+
+bala.count("hihh")
+
+
+# In[40]:
+
+
+lst=[1,12,'bl',[43,8]]
+
+
+# In[41]:
+
+
+type(lst)
+
+
+# In[42]:
+
+
+print(lst)
+
+
+# In[43]:
+
+
+lst.clear()
+
+
+# In[44]:
+
+
+lst
+
+
+# In[45]:
+
+
+print(lst)
+
+
+# In[46]:
+
+
+lst=[1,12,'bl',[43,8]]
+
+
+# In[47]:
+
+
+lst
+
+
+# In[48]:
+
+
+ho={"ho1":"fefef","ho2":"hee"}
+
+
+# In[49]:
+
+
+ho.get("ho1")
+
+
+# In[50]:
+
+
+hi={1:123,2:12334}
+
+
+# In[51]:
+
+
+hi.get(1)
+
+
+# In[1]:
+
+
+a=1
+b=3
+a>=b
+
+
+# In[2]:
+
+
+a=2
+b=4
+c=5
+
+
+# In[3]:
+
+
+b==c
+
+
+# In[4]:
+
+
+a<b or b=c 
+
+
+# In[5]:
+
+
+if a<b or b=c :
+    print("true")
+
+
+# In[8]:
+
+
+if a<b or b==c :
+    print("not false")
+
+
+# In[9]:
+
+
+print("hellow       world")
+
+
+# In[10]:
+
+
+print("""38999*&#^&#*#""")
+
+
+# In[11]:
+
+
+print("hellow's arror")
+
+
+# In[12]:
+
+
+print ("hellow\t bla")
+
+
+# In[13]:
+
+
+print ("hello \n bala")
+
+
+# In[14]:
+
+
+print ("hellow\ bla")
+
+
+# In[17]:
+
+
+bala=1
+b=4
+print (f"hi this is bala {bala} {b}")
+
+
+# In[19]:
+
+
+bala="bakalahdjd"
+b=4
+print (f"hi this is bala {bala} {b}")
+
+
+# In[22]:
+
+
+a="bakalahdjd"
+b=4
+print (f"hi this {0} and i am from {1}",a,b )
+
+
+# In[21]:
+
+
+a=1
+b=1.34
+print (f"hiisfin{a}\,jnfifiusf{b}w")
+
+
+# In[24]:
+
+
+a="bakalahdjd"
+b=4
+print (f"hi this {a} and i am from {b}",a,b )
+
+
+# In[25]:
+
+
+a=2**3
+
+
+# In[26]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+
+
