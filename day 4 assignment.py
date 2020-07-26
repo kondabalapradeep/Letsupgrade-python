bed3bc435a7a42855613208572ee217be6c84a52
kondabalapradeep <68560029+kondabalapradeep@users.noreply.github.com>
2020-07-27 02:57:47 +0530

    Add files via upload

---
+++ day 4 assignment.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 1
+
+# In[1]:
+
+
+b="What we become;we are python programmer"
+i=0
+while True:
+    i=b.find("we",i,len(b)-1)
+    if i==-1:
+        break
+    else:
+        print(i)
+        i=i+1
+
+
+# # Assignment 2
+
+# In[2]:
+
+
+a="abcd"
+
+
+# In[3]:
+
+
+a.upper()
+
+
+# In[4]:
+
+
+b="ABCD"
+
+
+# In[5]:
+
+
+b.lower()
+
+
+# In[ ]:
+
+
+
+
