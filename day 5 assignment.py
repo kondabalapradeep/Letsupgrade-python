0dd74caf8f5d3437e268a3b7d637297195ac953d
kondabalapradeep <68560029+kondabalapradeep@users.noreply.github.com>
2020-07-29 00:35:58 +0530

    Add files via upload

---
+++ day 5 assignment.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+list1=[10,20,40,60,70,80]
+list2=[5,15,25,35,45,60]
+a={}
+for i in range(0,len(list1)):
+    a[list1[i]]=list2[i]
+print(a)    
+
+
+# In[21]:
+
+
+for i in reversed(range(0,len(list1))):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
